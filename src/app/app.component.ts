7a75034200b8a107060d2f68f0a9aa446fff32c9
bilinskip <p.bilinski22@gmail.com>
2020-03-21 17:51:38 +0100

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Task} from './task';
+
 
 @Component({
   selector: 'my-app',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
     config: { [key:string]: string | Date } = null;
+
+    tasks: Task[] = [{
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false
+    },
+    {
+      name: 'Nauka',
+      deadline: '2020-03-10',
+      done: false
+    },
+    {
+      name: 'Sprzątanie',
+      deadline: '2020-03-21',
+      done: false
+    }
+    ];
 
     constructor(){
       setTimeout( () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { registerLocaleData } from '@angular/common';
+
+import localePl from '@angular/common/locales/pl';
+registerLocaleData(localePl);
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
